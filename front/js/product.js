b023682afeb72578ee0ea5a97328587cd8bd07d7
Philippe Cohen <cohen.philippe@hotmail.com>
2022-06-28 15:04:48 +0200

    Revert "bug(script) : counter cart"
    This reverts commit 259f87af191a0f7c0dc2568b04597020a6530ad9.

--- front/js/product.js
+++ front/js/product.js
@@ -14,7 +14,7 @@ const api = 'http://localhost:3000/api/products/' + id;
  * Connection with API with GET for reception
  * @param {string} api - url of api declared in "const api"
  * @returns {Promise.resolve<string>} - array of product and detail product
- * @returns {Promise.reject<Error>} - Connection error or bad request error
+ * @returns {Promise.reject<Error>} - knex Err or BadRequestError
  */
  fetch(api)
 
@@ -38,29 +38,40 @@ const api = 'http://localhost:3000/api/products/' + id;
         ;
 
         //HTML product's title generation
-        title.innerText = value.name;
+        document
+            .querySelector("#title")
+            .innerText = value.name
+        ;
         
         //HTML price generation
-        price.innerText = value.price;
+        document
+            .querySelector("#price")
+            .innerText = value.price
+        ;
             
         //HTML description generation
-        description.innerHTML = value.description;
+        document
+            .querySelector("#description")
+            .innerHTML = value.description
+        ;
         
         //HTML color's choice generation
         for (let color of value.colors) {
-            colors.innerHTML = `<option value="${color}">${color}</option>`
+            document
+                .querySelector("#colors")
+                .innerHTML = `<option value="${color}">${color}</option>`
                 + document.querySelector("#colors").innerHTML
             ;
-        };
+        }
     };
  })
 
 // If error, display it on console and display a alert
 .catch((err) => {
     console.log(err);
-    alert("la connexion avec l\'API à échoué")
-});
-
+    alert("la connexion avec l\'API a échoué")
+})
+;
 
 
 /************************************************************ */
@@ -68,33 +79,42 @@ const api = 'http://localhost:3000/api/products/' + id;
 /************************************************************ */
 
 //Initialisation of object for store product's detail
-let purchase = {
+let product = {
     "id": id,
+    "quantity": '',
     "color" : '',
-    "quantity" : ''
 };
 
 
 //Set Object product : Quantity
-quantity.addEventListener('input', (e) => purchase.quantity = e.target.value);
+document
+    .querySelector('#quantity')
+    .addEventListener('input', (e) => product.quantity = e.target.value)
+;
 
 
 //Set Object product : Color
-colors.addEventListener('input', (e) => purchase.color = e.target.value);
-
-
+document
+    .querySelector('#colors')
+    .addEventListener('input', (e) => product.color = e.target.value)
+;
 
 /**************************************************************/
 /*                     ADD CART BUTTON                        */
 /**************************************************************/
 
 
-// When click on button, save in local storage and go to cart's page
-addToCart.addEventListener('click', function (){
-    if (purchase.quantity >= 1 && purchase.color != '') {
-        addCart(purchase);
-        document.location.href = "./cart.html";
-    } else {
-        alert('Veuillez choisir une couleur et une quantité')
-    }
-});
+// When it have a click on a button, save in local storage and go to cart's page
+document
+    .querySelector('#addToCart')
+    .addEventListener('click', function (){
+        if (product.quantity >= 1 && product.color != '') {
+            addCart(product);
+            document.location.href = "http://127.0.0.1:5500/front/html/cart.html";
+        } else {
+            alert('Veuillez choisir une couleur et une quantité')
+        }
+    })
+;
+
+let cart = getCart();
--- front/js/script.js
+++ front/js/script.js
@@ -2,29 +2,8 @@
 /*     GLOBAL FUNCTION        */
 /**************************** */
 
-
-/**
- * Recovery purchase and convert it in a array for cart
- * @param {any} purchase - Object who content article purchase with user detail's choice (color and quantity)
- * @returns {array | undefined} - Cart with purchase article in array format or undifined if purchase wasn't done
- */
- function convertInCartArray(purchase) {
-
-    //start function only if purchase done
-    if (purchase.color != null && purchase.quantity != null) {
-        //generate cart array
-        return [
-            productOption = {
-                "id" : purchase.id,
-                "color" : purchase.color,
-                "quantity" : purchase.quantity
-            }
-        ];
-    } else {
-        console.log('convert failed, purchase was not define');
-    }
-}
-
+//Recovery of id from url
+// let id = new URL(document.location.href).searchParams.get("id");
 
 
 /**
@@ -32,66 +11,24 @@
  * @param {any} cart - Object : article purchase with color, id and quantity detail
  * @returns {string} - Save cart in locale storage
  */
- function saveCart(cart) {
-    //Verify is cart was defined
-    if(cart != undefined && cart.length != 0 ) {
-        localStorage.setItem('cart', JSON.stringify(cart));
-    } else {
-        console.log('Product undefined or empty cart, save abort');
-    }
-    //Update cart from local storage
-    cart = JSON.parse(localStorage.getItem('cart'));
-    console.log('save');
-    console.log(cart);
+function saveCart(cart) {
+    localStorage.setItem("cart", JSON.stringify(cart));
 }
-
 
 
 /**
  * Recovery cart from local storage
- * @returns {array} - return a cart array from local storage
+ * @returns {string | array} - If cart exist return empty array else return existing cart from local storage in array format
  */
 function getCart() {
-    //Recovery of cart
     let cart = localStorage.getItem('cart');
-
-    //If cart doesn't exist, convert purchase in cart array
-    if (cart == null && purchase.color != '' && purchase.quantity != '') {
-        cart = [];
-        console.log('getCart() null');
-        return cart;
-    //Else if cart exist return it
-    } else if (cart != null) {
-        console.log('existing cart')
-        cartReturn = JSON.parse(cart);
-        //If cart in an array, return it
-        if(Array.isArray(cartReturn) === true) {
-            console.log('getCart() array=true');
-            return cartReturn;
-        //Else convert it
-        } else {
-            console.log('getCart() array=false');
-            return convertInCartArray(cartReturn);
-        }
-    //Else do nothing
+    //Create cart if doesn't exist, else parse local storage element
+    if (cart == null) {
+        return [];
     } else {
-        console.log('incomplete purchase, recovery cart aborted')
+        return JSON.parse(cart);
     }
 }
-
-
-
-// /**
-//  * Search if product is in a cart
-//  * @param {array} purchase - Article purchase from product page
-//  * @returns {array | undefined} - Return article's array if it's found, "undefined" if doesn't found
-//  */
-//  function searchProduct(purchase) {
-//     let cart = getCart();
-//     let foundResult = cart.find(p => p[0] == purchase.id);   
-//     return foundResult;
-// };
-
 
 
 /**
@@ -99,101 +36,69 @@ function getCart() {
  * @param {any} product - Object : article purchase with color, id and quantity detail
  * @returns {string} - Function "saveCart" : save product in cart in local storage
  */
-function addCart(purchase) {
-    //recovery cart
+function addCart(product) {
+    //Recovery of cart
     let cart = getCart();
-    console.log(cart);
-    //If cart is empty
-    if (cart.length == 0) {
-        console.log('empty array');
-        cart.push(purchase);
-    } else {       
-        //For each product of cart
-        for (let kanap in cart) {
-            //If a same id and color with purchase
-            if  (kanap.id === purchase.id && kanap.color === purchase.color) {
-                // kanap.quantity = kanap.quantity + purchase.quantité  
-                let sum = parseInt(kanap.quantity) + parseInt(purchase.quantity);
-                console.log('kanap.quantity :');
-                console.log(kanap.quantity);
-                console.log('purchase.quantity');
-                console.log(purchase.quantity);           
-                
-                //Add quantity of this product in cart               
-                kanap.quantity = parseInt(sum);
-                console.log('SUM = ');
-                console.log(sum);                
-            //Else add purchase at cart array
-            } else if (kanap.id != purchase.id || kanap.color != purchase.color) {
-                cart.push(purchase);
-            }    
-        console.log('addCart');
-        console.log(cart);
+    //Search the good product by id
+    let foundProduct = cart.find(c => c.id == product.id);
+    console.log("found : " + foundProduct);
+    //If product was found
+    if (foundProduct != undefined) {
+        console.log('produit trouvé');
+        //add quantity if it have a same color
+        if (foundProduct.color != product.color) {
+            cart.color = [];
+            cart.color.push(product.color)
+            cart.quantity += product.quantity;
+            console.log('j ajoute la quantité et la couleur');
+        //Else add a new product in cart
+        } else {
+            cart.quantity += product.quantity;
+            console.log('j ajoute la quantité :' + cart.quantity);
         }
+    } else {
+        cart = Object.assign({}, product);
+        console.log('produit non trouvé je l ajoute au panier')
     }
-
-    // //If product was found and it have a same color
-    // if (foundProduct != undefined && foundProduct[1] == purchase.color) {        
-    //     //add quantity
-    //     foundProduct[2] = parseInt(foundProduct[2]) + parseInt(purchase.quantity);
-    
-    // //Else add a new product in cart 
-    // } else {        
-    //     //convert cart in array
-    //     console.log('produit non trouvé ou couleur différente');
-
-    //     //recover product
-    //     foundProduct = Object.assign({}, product);
-    //     //convert cart in array
-    //     let cartArray = Object.entries([getCart()]);
-    //     //add product in cart array
-    //     cartArray.push([foundProduct]);
-
-    //     console.log('cartArray');
-    //     console.log(cartArray);
-        
-    //     //convert cart in object
-    //     foundProduct = {...cartArray};
-        
-    //     console.log('pas la même couleurs');
-    //     console.log(foundProduct);
-    // };
-    
-    //Save product in locale storage    
+    //Save product in locale storage
     saveCart(cart);
-};
+    console.log('produit sauvegarder dans le panier')
+}
+
+
+/**
+ * Remove product from cart
+ * @param {any} product - Object : article purchase with color, id and quantity detail
+ * @returns {string} - Function "saveCart" : save cart in local storage
+ */
+function removeFromCart(product) {
+    let cart = getCart();
+    cart = cart.filter(cart => cart.id != product.id);
+    saveCart(cart);
+}
+
+
+ /**
+  * Change quantity of product from cart
+  * @param {any} product - object : article purchase with color, id and quantity detail
+  * @param {int} quantity - number of quantity where we want add
+  * @returns {any}
+  */
+ function changeQuantity(product, quantity) {
+    let cart = getCart();
+    //add quantity of product
+    let foundProduct = cart.find(cart => cart.id == product.id);
+    if (foundProduct != undefined) {
+        foundProduct.quantity += quantity;
+        if (foundProduct.quantity <= 0) {
+            removeFromCart(foundProduct);
+        } else {
+            saveCart(cart);
+        }
+    } 
+}
 
 
 
-// /**
-//  * Remove product from cart
-//  * @param {any} product - Object : article purchase with color, id and quantity detail
-//  * @returns {string} - Function "saveCart" : save cart in local storage
-//  */
-// function removeFromCart(product) {
-//     let cart = getCart();
-//     cart = cart.filter(cart => cart.id != product.id);
-//     saveCart(cart);
-// };
 
 
-
-//  /**
-//   * Change quantity of product from cart
-//   * @param {any} product - object : article purchase with color, id and quantity detail
-//   * @param {int} quantity - number of quantity where we want add
-//   * @returns {any}
-//   */
-//  function changeQuantity(product, quantity) {
-//     let cart = getCart();
-//     //add quantity of product
-//     let foundProduct = cart.find(cart => cart.id == product.id);
-//     if (foundProduct != undefined) {
-//         foundProduct.quantity += quantity;
-//         if (foundProduct.quantity <= 0) {
-//             removeFromCart(foundProduct);
-//         } else {
-//             saveCart(cart);
-//         };
-//     };
-// };

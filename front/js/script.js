d10a7247d5c8ff7c04c42ada10d1ced9d7cd815d
Philippe Cohen <cohen.philippe@hotmail.com>
2022-06-28 15:26:28 +0200

    Revert "feat: script js, functions for cart, add, remove, change quantity, ghet and save"
    This reverts commit 01984f47d03a940be487aa9341b458440cacc50a.

--- front/js/script.js
+++ front/js/script.js
@@ -1,104 +1,75 @@
-/**************************** */
-/*     GLOBAL FUNCTION        */
-/**************************** */
+/******************/ 
+/* Page d'Accueil */
+/*****************/
 
 //Recovery of id from url
 // let id = new URL(document.location.href).searchParams.get("id");
 
 
-/**
- * Save cart in locale storage
- * @param {any} cart - Object : article purchase with color, id and quantity detail
- * @returns {string} - Save cart in locale storage
- */
-function saveCart(cart) {
-    localStorage.setItem("cart", JSON.stringify(cart));
-}
+//Lien API
+const api = 'http://localhost:3000/api/products';
+
+//Stockage des valeurs
+let kanapData = '';
 
 
-/**
- * Recovery cart from local storage
- * @returns {string | array} - If cart exist return empty array else return existing cart from local storage in array format
- */
-function getCart() {
-    let cart = localStorage.getItem('cart');
-    //Create cart if doesn't exist, else parse local storage element
-    if (cart == null) {
-        return [];
-    } else {
-        return JSON.parse(cart);
-    }
-}
+/** Connexion avec l'API en GET pour reception */
 
-
-/**
- * Add product in cart (local strorage)
- * @param {any} product - Object : article purchase with color, id and quantity detail
- * @returns {string} - Function "saveCart" : save product in cart in local storage
- */
-function addCart(product) {
-    //Recovery of cart
-    let cart = getCart();
-    //Search the good product by id
-    let foundProduct = cart.find(c => c.id == product.id);
-    console.log("found : " + foundProduct);
-    //If product was found
-    if (foundProduct != undefined) {
-        console.log('produit trouvé');
-        //add quantity if it have a same color
-        if (foundProduct.color != product.color) {
-            cart.color = [];
-            cart.color.push(product.color)
-            cart.quantity += product.quantity;
-            console.log('j ajoute la quantité et la couleur');
-        //Else add a new product in cart
-        } else {
-            cart.quantity += product.quantity;
-            console.log('j ajoute la quantité :' + cart.quantity);
+fetch(api)
+    // vérification de la connexion
+    .then ((res) => {
+        if (res.ok) {
+            return res.json();
         }
-    } else {
-        cart = Object.assign({}, product);
-        console.log('produit non trouvé je l ajoute au panier')
-    }
-    //Save product in locale storage
-    saveCart(cart);
-    console.log('produit sauvegarder dans le panier')
-}
+    })
+    //on récupère les données et on les enregistre dans la variable kanapData
+    .then((value) => {
+        kanapData = value;
 
+        /** On affiche les produits récupéré de l'API sur la page d'accueil */
 
-/**
- * Remove product from cart
- * @param {any} product - Object : article purchase with color, id and quantity detail
- * @returns {string} - Function "saveCart" : save cart in local storage
- */
-function removeFromCart(product) {
-    let cart = getCart();
-    cart = cart.filter(cart => cart.id != product.id);
-    saveCart(cart);
-}
+        for (let kanap of kanapData) {
+            //Génération du code HTML
+            document
+                .getElementById('items')
+                .innerHTML = `<a href="">
+                                <article>
+                                    <img src="" alt="">
+                                    <h3 class="productName"></h3>
+                                    <p class="productDescription"></p>
+                                </article>
+                            </a>`+ document.getElementById('items').innerHTML;                   
 
+            //Lien
+            document
+                .querySelector('section a')
+                .setAttribute('href', `./product.html?id=${kanap._id}`);
 
- /**
-  * Change quantity of product from cart
-  * @param {any} product - object : article purchase with color, id and quantity detail
-  * @param {int} quantity - number of quantity where we want add
-  * @returns {any}
-  */
- function changeQuantity(product, quantity) {
-    let cart = getCart();
-    //add quantity of product
-    let foundProduct = cart.find(cart => cart.id == product.id);
-    if (foundProduct != undefined) {
-        foundProduct.quantity += quantity;
-        if (foundProduct.quantity <= 0) {
-            removeFromCart(foundProduct);
-        } else {
-            saveCart(cart);
-        }
-    } 
-}
+            //Image
+            document
+                .querySelector('article img')
+                .setAttribute('src', kanap.imageUrl);
+            document
+                .querySelector('article img')
+                .setAttribute('alt', kanap.altTxt);
 
+            //Nom
+            document
+                .querySelector('article h3')
+                .innerText = kanap.name;
 
+            //Description
+            document
+                .querySelector('article p')
+                .innerText = kanap.description;
+        };
+    })
 
-
-
+    // Si erreur on l'affiche dans la console et on affiche un message sur la page d'accueil 
+    .catch((err) => {
+        console.log(err);
+        document
+            .getElementById('items')        
+            .innerText = "La connection avec l'API à échoué :( ";
+    });
+    

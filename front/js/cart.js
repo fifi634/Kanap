efcb55bb668d9827610b4802d9cc468932513391
Philippe Cohen <cohen.philippe@hotmail.com>
2022-07-03 22:03:05 +0200

    feat(cart.js) : empty cart message ok + all display cart ok

--- front/js/cart.js
+++ front/js/cart.js
@@ -26,7 +26,7 @@ let clientOrder = {
 
 
 // If cart exist in local storage
-if (localStorage.getItem('cart') != null) {
+if (cart != null && cart.length > 0) {
     //for each product of cart
     for (let kanap of cart) {
         let kanapId = kanap[0];
@@ -35,61 +35,64 @@ if (localStorage.getItem('cart') != null) {
 
         // Connection with API
         fetch(api)
-        //Check API's connection
-        .then ((response) => response.json())
-        //Work on reception's value
-        .then((value) => {
-            // For each product of cart
-            for (let product of value) {
-                // If article purchase was found in API data
-                if (kanapId == product._id) {
-                    cart__items.innerHTML += 
-                        `<article class="cart__item" data-id="${kanapId}" data-color="${kanapColor}">
-                            <div class="cart__item__img">
-                                <img src="${product.imageUrl}" alt="${product.altTxt}">
-                            </div>
-                            <div class="cart__item__content">
-                                <div class="cart__item__content__description">
-                                    <h2>${product.name}</h2>
-                                    <p>${kanapColor}</p>
-                                    <p>${product.price}€</p>
+            //Check API's connection
+            .then ((response) => response.json())
+            //Work on reception's value
+            .then((value) => {
+                // For each product of cart
+                for (let product of value) {
+                    // If article purchase was found in API data
+                    if (kanapId == product._id) {
+                        cart__items.innerHTML += 
+                            `<article class="cart__item" data-id="${kanapId}" data-color="${kanapColor}">
+                                <div class="cart__item__img">
+                                    <img src="${product.imageUrl}" alt="${product.altTxt}">
                                 </div>
-                                <div class="cart__item__content__settings">
-                                    <div class="cart__item__content__settings__quantity">
-                                        <p>Qté : </p>
-                                        <input type="number" class="itemQuantity" name="itemQuantity" onchange="changeQuantity('${kanapId}', '${kanapColor}', this.value)" min="1" max="100" value="${kanapQty}">
+                                <div class="cart__item__content">
+                                    <div class="cart__item__content__description">
+                                        <h2>${product.name}</h2>
+                                        <p>${kanapColor}</p>
+                                        <p>${product.price}€</p>
                                     </div>
-                                    <div class="cart__item__content__settings__delete">
-                                        <p class="deleteItem" onclick="deleteProduct('${kanapId}', '${kanapColor}')">Supprimer</p>
+                                    <div class="cart__item__content__settings">
+                                        <div class="cart__item__content__settings__quantity">
+                                            <p>Qté : </p>
+                                            <input type="number" class="itemQuantity" name="itemQuantity" onchange="changeQuantity('${kanapId}', '${kanapColor}', this.value)" min="1" max="100" value="${kanapQty}">
+                                        </div>
+                                        <div class="cart__item__content__settings__delete">
+                                            <p class="deleteItem" onclick="deleteProduct('${kanapId}', '${kanapColor}')">Supprimer</p>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                        </article> `
-                    ;
+                            </article> `
+                        ;
 
-                    // Calculate total price and display it 
-                    price += parseInt(product.price, 10) * parseInt(kanapQty, 10);
-                    document
-                        .querySelector('#totalPrice')
-                        .innerText = price
-                    ;
-                    
-                // Else if empty cart, write it
-                } else {
-                    console.log('empty cart');
-                    document.getElementsByTagName('h1').innerText = "Votre panier est vide";
-                    document.querySelector('.cart__order') = '';
-                    document.querySelector('cart__price') = '';
+                        // Calculate total price and display it 
+                        price += parseInt(product.price, 10) * parseInt(kanapQty, 10);
+                        document
+                            .querySelector('#totalPrice')
+                            .innerText = price
+                        ;
+                    }
                 }
-            }
-        })
+            })
 
-        // If error, display it on console and display a alert
-        .catch((err) => {
-            console.log(err);
-            document
-                .querySelector('#order')
-                .setAttribute('value', "L'API a rencontré une erreur, plus d'info dans la console.")
-        });      
+            // If error, display it on console and display a alert
+            .catch((err) => {
+                console.log(err);
+                document
+                    .querySelector('#order')
+                    .setAttribute('value', "L'API a rencontré une erreur, plus d'info dans la console.")
+            })
+        ;      
     }
+} else // Else if empty cart, write it
+{
+    console.log('empty cart');
+    document.querySelector('h1').innerText = "Votre panier est vide";
+    document.querySelector('.cart__price').innerText = "";
+    // Button for return in store
+    document.querySelector('.cart__order')
+        .innerHTML = '<a href=http://' + window.location.host + '/front/html/><input type="button" value="Retourner au magasin ?"/></a>'
+    ;
 }
